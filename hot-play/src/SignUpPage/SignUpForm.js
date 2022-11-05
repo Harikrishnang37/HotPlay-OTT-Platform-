bc743b5c1f33e4b6c497bd8def43d070d0545681
HiteshYadav007 <hiteshyadavm007@gmail.com>
2022-11-05 13:06:33 +0530

    Added CSS files

--- hot-play/src/SignUpPage/SignUpForm.js
+++ hot-play/src/SignUpPage/SignUpForm.js
@@ -7,11 +7,11 @@ export default function SignUpForm (props)
         <div className='signUp-box'>
             <h2> Sign Up</h2>
             <form className='signUp-form'>
-                Email: 
+                <label>Email:</label> 
                 <input type = "email" placeholder='email'/> <br/>
-                Password: 
+                <label>Password:</label> 
                 <input type = "password" placeholder='password'/> <br/>
-                Confirm Password:
+                <label>Password:</label>
                 <input type = "password" placeholder='confirm password'/> <br/>
 
                 <input type = "checkbox" className='TC'/>I agree to the terms and Conditions <br/>
